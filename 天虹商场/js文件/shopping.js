02c0e64be25d1ecf957c160198c76a5bee565e0f
unknown <TIANTANGB@163.com>
2019-12-11 14:07:11 +0800

    购物车修改

--- 天虹商场/js文件/shopping.js
+++ 天虹商场/js文件/shopping.js
@@ -1,81 +1,102 @@
-class Car{
-    constructor(options){
+
+class Car {
+    constructor(options) {
         this.url = options.url;
         this.tbody = options.tbody;
+        // 1.读取到所有商品数据
         this.load();
+        // 5.事件委托绑定事件
         this.addEvent();
-        
+        //this.changs();
+    //his.checkLine();
     }
-    load(){
+    load() {
         var that = this;
-        ajaxGet(this.url,function(res){
+        ajaxGet(this.url, function (res) {
             that.res = JSON.parse(res);
+            // 2.读取到cookie
             that.getCookie();
         })
     }
-    getCookie(){
+    getCookie() {
         this.goods = getCookie("goods") ? JSON.parse(getCookie("goods")) : [];
+
+        // 3.拿到cookie中的id与所有商品数据的goodsId做比较
         this.display();
     }
-   display(){
-         var str = "";
-         for(var i=0;i<this.res.length;i++){
-             for(var j=0;j<this.goods.length;j++){
-                 if(this.res[i].goodsId === this.goods[j].id){
-                     str += `<tr index="${this.goods[j].id}">
-                                <td><input type="checkbox" class="te"></td>
+    display() {
+        var str = "";
+        for (var i = 0; i < this.res.length; i++) {
+            for (var j = 0; j < this.goods.length; j++) {
+                if (this.res[i].goodsId === this.goods[j].id) {
+                    // 4.相同了，渲染这个数据（就是添加到购物车的商品）
+                    str += `<tr index="${this.goods[j].id}">
+                                <td><input type="checkbox" class="checkLine" id="checkAll"></td>
                                 <td><img src="${this.res[i].img}" alt=""></td>
-                                <td><p>${this.res[i].name}</p></td>
-                                <td>${this.res[i].price}</td>
-                                <td></td>
-                                <td><input type="number""${this.goods[j].num} min="1"></td>
+                              <td><p>${this.res[i].name}</p></td>
+                                <td><i>${this.res[i].price}</i></td>
+                                <td><input type="number"value="${this.goods[j].num}" min="1" class="l"></td>
                                 <td><span>删除</span></td>
-                            </tr>`
-                    }
-             }
-         }
+                                <td><s>${parseInt(this.res[i].price.slice(1,5)) * this.goods[j].num}</s></td>
+                                </li>`
+                            }
+                            // console.log(s)
+                            // console.log(parseInt(this.res[i].price.slice(1,5)))
 
-         this.tbody.innerHTML = str;
+
+            }
+        }
+        this.tbody.innerHTML = str;
     }
-    addEvent(){
+    addEvent() {
         var that = this;
-        this.tbody.addEventListener("click",function(eve){
-            if(eve.target.tagName == "SPAN"){
+        // 5-1.委托删除的事件
+        this.tbody.addEventListener("click", function (eve) {
+            if (eve.target.tagName == "SPAN") {
+                // 6-1.保存要删除的数据的id
                 that.id = eve.target.parentNode.parentNode.getAttribute("index");
+                // 7-1.删除DOM元素
                 eve.target.parentNode.parentNode.remove();
-                that.removeCookie();
+                // 8-1.调用更新cookie方法，传入删除操作
+                // 是真正的删除cookie么？不是
+                // 因为只有一条cookie，里面的数据是数组的形式
+                // 从这条cookie中剔除出这个数据
+                that.updateCookie(function (i) {
+                    that.goods.splice(i, 1);
+                });
             }
         })
-        this.tbody.addEventListener("input",function(eve){
-            if(eve.target.tagName == "INPUT"){
-                that.id = eve.target.parentNode.parentNode.getAttribute("index");
-                that.value = eve.target.value;
-                that.changeCookie();
-                that.updataCookir(function(i){
+        // 5-2.委托修改的事件
+        this.tbody.addEventListener("input", function (eve) {
+            if (eve.target.tagName == "INPUT") {
+                // 6-2.保存要修改的数据的id
+                that.id = eve.target.parentNode.getAttribute("index");
+                // 7-2.调用更新cookie的方法，传入修改操作
+                that.updateCookie(function (i) {
                     that.goods[i].num = eve.target.value;
-                })
+                });
+                eve.target.parentNode.parentNode.querySelector("s").innerHTML = parseInt (eve.target.parentNode.parentNode.querySelector("i").innerHTML.slice(1,5))* parseInt (eve.target.value);
+            //    var t = parseInt (eve.target.parentNode.parentNode.querySelector("i").innerHTML.slice(1,5))* parseInt (eve.target.value)
+
+                
+                // console.log(eve.target.parentNode.parentNode.querySelector("s").innerHTML)
             }
+
         })
     }
-    removeCookie(){
-        for(var i=0;i<this.goods.length;i++){
-            if(this.goods[i].id === this.id){
-                this.goods.splice(i,1);
+
+    updateCookie(cb) {
+        for (var j = 0;  j< this.goods.length; j++) {
+            if (this.goods[j].id === this.id) {
+                cb(j);
             }
         }
-        setCookie("goods",JSON.stringify(this.goods))
-    }
-    changeCookie(){
-        for(var i=0;i<this.goods.length;i++){
-            if(this.goods[i].id === this.id){
-                this.goods[i].num =this.value;
-            }
-        }
-        setCookie("goods",JSON.stringify(this.goods))
+        setCookie("goods", JSON.stringify(this.goods))
     }
 
 }
+
 new Car({
     url:"http://localhost/天虹商场/json文件/shangpinliebiao.json",
-    tbody:document.querySelector("tbody")
+        tbody:document.querySelector("tbody")
 })
